5ce9c6fcddedc1811bb92608239aea38cf85050d
Guilherme Gehring <guilherme.gehring@gmail.com>
2019-06-17 08:43:56 -0300

    Correção na identação

--- src/java/br/edu/ifsul/dao/AlunoDAO.java
+++ src/java/br/edu/ifsul/dao/AlunoDAO.java
@@ -16,21 +16,23 @@ import javax.ejb.Stateful;
  * @author 20171pf.cc0178
  */
 @Stateful
-public class AlunoDAO extends DAOGenerico<Aluno> implements Serializable{
+public class AlunoDAO extends DAOGenerico<Aluno> implements Serializable {
 
     public AlunoDAO() {
         super(Aluno.class);
     }
-        
+
     @Override
     public Aluno getObjectById(Object id) throws Exception {
         List<Aluno> alunos = getListaObjetos();
         System.out.println("ID: " + id);
-        for(Aluno aluno : alunos)
-            if(Objects.equals(aluno.getId(), id))
+        for (Aluno aluno : alunos) {
+            if (Objects.equals(aluno.getId(), id)) {
                 return aluno;
+            }
+        }
         return alunos.get(0);
-        
-    }   
-    
+
+    }
+
 }
